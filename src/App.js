35e3cb2b3ce9052ccd83d0299863fb2e209c1861
KaustubhDalal <kaustubhvd5@gmail.com>
2023-07-29 22:12:26 +0530

    enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import PropTypes from 'prop-types'
@@ -8,7 +9,8 @@ function App() {
     <>
       <Navbar aboutText="About Us"/>
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below:"/>
+      {/* <TextForm heading="Enter the text to analyze below:"/> */}
+      <About/>
       </div>
     </>
   );
